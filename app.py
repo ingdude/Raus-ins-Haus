7e2b5a38749b2a913124131fd50bfcf3d4bd9a5b
ingdude <korber93@gmail.com>
2026-04-10 12:47:36 +0200

    Change column layout to 3 columns in display

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ if menu == "🏠 Übersicht":
         # Wir zeigen die Kacheln an
         for index, row in display_df.iterrows():
             with st.container(border=True):
-                col1, col2 = st.columns()
+                col1, col2 = st.columns(3)
                 
                 with col1:
                     if pd.notnull(row["Bild-URL"]) and str(row["Bild-URL"]).startswith("http"):
